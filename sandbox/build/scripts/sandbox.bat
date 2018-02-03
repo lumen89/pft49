dd0151ec8bff55b3574614818b845497d7d68460
lumen1989 <Kalinin499@gmail.com>
2018-02-03 21:17:33 +0300

    edit mainClassName in build.gradle and rerun it

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -69,10 +69,10 @@ set CMD_LINE_ARGS=%$
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\sandbox.jar
+set CLASSPATH=%APP_HOME%\lib\sandbox.jar;%APP_HOME%\lib\testng-6.13.1.jar;%APP_HOME%\lib\jcommander-1.72.jar
 
 @rem Execute sandbox
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" ru.stqa.pft.sanbox.HW %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" ru.stqa.pft.sandbox.MainProgramClass %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
