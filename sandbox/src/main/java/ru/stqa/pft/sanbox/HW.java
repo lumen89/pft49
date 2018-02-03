193400406af44d27d2e05413daaf1b3357f306db
lumen1989 <Kalinin499@gmail.com>
2018-02-03 19:05:26 +0300

    rename package

--- sandbox/src/main/java/ru/stqa/pft/sanbox/HW.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/HW.java
@@ -4,8 +4,8 @@ public class HW {
     public static void main(String[] args) {
 
 
-        Point p1 = new Point(2,2);
-        Point p2 = new Point(5,5);
+        Point p1 = new Point(1,4);
+        Point p2 = new Point(10,15);
         System.out.println("The distance between two points is " + p1.distance(p2));
 
 
