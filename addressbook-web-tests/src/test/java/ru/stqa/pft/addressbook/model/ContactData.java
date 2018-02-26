cfef96b07ecb79ea87b952d3a636043197564fcb
lumen1989 <Kalinin499@gmail.com>
2018-02-26 20:08:23 +0300

    add methods (equals and hashCode) to compare elements in list for contactDeletionTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.model;
 
 
+import java.util.Objects;
+
 public class ContactData {
     private final String firstname;
     private final String middlename;
@@ -124,4 +126,25 @@ public class ContactData {
     }
 
     public String getGroup() { return group; }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(lastname, that.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(lastname);
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -11,15 +11,19 @@ public class ContactDeletionTests extends TestBase {
     @Test
     public void testDeletionContact() {
         app.getNavigationHelper().goToHomePage();
-        List<ContactData> before = app.getContactHelper().getContactList();
         if(! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("first name 1", "middle name 1", "last name 1", "nickname 1", "title 1", "company 1", "address 1", "9999999", "8888888", "77777777", "6666666", "email1@test.com", "email2@test.com", "email3@test.com", "localhost.com", "1988", "2015", "secondary address 1", "test notes", "edit_group 1"), true);
         }
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().initContactEdition(before.size() - 1);
         app.getContactHelper().deleteSelectContacts();
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size());
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
+
 
     }
 
